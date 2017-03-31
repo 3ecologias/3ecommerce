2ecd671bffc67525ea04e6a4cdc80de1275a1bd0
Ricardo Brazileiro <rbrazileiro@gmail.com>
2017-03-31 19:16:04 -0300

    update grid

--- core/static/js/init.js
+++ core/static/js/init.js
@@ -169,4 +169,20 @@ jQuery(document).ready(function($) {
    });
 
 
+   /*----------------------------------------------------*/
+   /*	https://github.com/desandro/masonry/
+   /* Mansory
+   ------------------------------------------------------ */
+    var $grid = $('.grid').masonry({
+      itemSelector: '.grid-item',
+      percentPosition: true,
+      columnWidth: '.grid-sizer'
+    });
+    // layout Isotope after each image loads
+    $grid.imagesLoaded().progress( function() {
+      $grid.masonry();
+    });
+
+
+
 });
