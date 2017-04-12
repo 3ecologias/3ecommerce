10a426b98e4f2bcd59159cecfc17cb75cd6febad
3Ecologias <root@3ecologias.net>
2017-04-12 15:52:18 -0300

    udapte

--- core/forms.py
+++ core/forms.py
@@ -4,7 +4,6 @@ from django.core.mail import send_mail, BadHeaderError
 from django.core.mail import EmailMessage
 
 
-
 class ContactUs(forms.Form):
 
     name = forms.CharField(required=True,
@@ -13,14 +12,34 @@ class ContactUs(forms.Form):
     email = forms.EmailField(required=True,
         widget=forms.EmailInput(attrs={'placeholder': 'Email *', 'id': 'email', 'class': 'form-control'}))
 
+    phone = forms.CharField(required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'Telefone', 'id': 'phone', 'class': 'form-control', 'type': 'tel'}))
+
+    itens = forms.CharField(required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'Itens no Catálogo', 'id': 'itens', 'class': 'form-control slider', 'type': 'text'}))
+
+    marketing = forms.CharField(required=False,
+        widget=forms.TextInput(attrs={'placeholder': '', 'id': 'marketing', 'class': 'form-control', 'type': 'checkbox', 'value': '1'}))
+
+    chat = forms.CharField(required=False,
+        widget=forms.TextInput(attrs={'placeholder': '', 'id': 'chat', 'class': 'form-control', 'type': 'checkbox', 'value': '1'}))
+
+    visual = forms.CharField(required=False,
+        widget=forms.TextInput(attrs={'placeholder': '', 'id': 'visual', 'class': 'form-control', 'type': 'checkbox', 'value': '1'}))
+
     message = forms.CharField(
-        required=True,
-        widget=forms.Textarea(attrs={'placeholder': 'Mensagem *', 'id': 'message', 'class': 'form-control', 'rows': '4'})
+        required=False,
+        widget=forms.Textarea(attrs={'placeholder': 'Observações *', 'id': 'message', 'class': 'form-control', 'rows': '3'})
     )
 
     def send_email(self):
         name = self.cleaned_data['name']
         from_email = self.cleaned_data['email']
+        phone = self.cleaned_data['phone']
+        itens = self.cleaned_data['itens']
+        marketing = self.cleaned_data['marketing']
+        chat = self.cleaned_data['chat']
+        visual = self.cleaned_data['visual']
         message = self.cleaned_data['message']
         subject = '[3ECOMMERCE - Novo Contato]'
 
